ea3a43cfba7b38ad59013325aa7d992dda68c54a
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2021-10-31 14:59:38 +0100

    Using stateful Lists...

--- mini-app-01/mini-app-01/src/App.js
+++ mini-app-01/mini-app-01/src/App.js
@@ -1,19 +1,24 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
 
 function App() {
-    const expenses = [
-        {title: 'Car Insurance 01', amount: 209.00, date: new Date(2021, 2, 28)},
-        {title: 'Car Insurance 02', amount: 259.00, date: new Date(2021, 6, 25)},
-        {title: 'Car Insurance 03', amount: 309.00, date: new Date(2021, 9, 28)},
-        {title: 'Car Insurance 04', amount: 401.10, date: new Date(2021, 12, 28)}
+    const INITIAL_EXPENSES = [
+        {id: 'e1',title: 'Car Insurance 01', amount: 209.00, date: new Date(2021, 2, 28)},
+        {id: 'e2' ,title: 'Car Insurance 02', amount: 259.00, date: new Date(2021, 6, 25)},
+        {id: 'e3' ,title: 'Car Insurance 03', amount: 309.00, date: new Date(2021, 9, 28)},
+        {id: 'e4' ,title: 'Car Insurance 04', amount: 401.10, date: new Date(2021, 12, 28)}
     ]
+
+    const [expenses, setExpenses] = useState(INITIAL_EXPENSES)
 
     const addExpenseHandler = expense => {
         console.log('In App.js')
         console.log(expense)
+        setExpenses((prevExpenses) => {
+            return [expense, ...prevExpenses]
+        })
     }
 
     return (
