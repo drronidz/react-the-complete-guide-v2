17dab2c10aa8e94ffade9fa039b744392b655fcd
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2021-11-01 14:42:58 +0100

    Mini-project 02: Adding Wrapper

--- mini-app-01/mini-app-02/src/components/Users/AddUser.jsx
+++ mini-app-01/mini-app-02/src/components/Users/AddUser.jsx
@@ -4,6 +4,7 @@ import './AddUser.css'
 import Card from "../UI/Card";
 import Button from "../UI/Button";
 import ErrorModal from "../UI/ErrorModal";
+import Wrapper from "../Helpers/Wrapper";
 
 const AddUser = (props) => {
     const [enteredUsername, setEnteredUsername] = useState('')
@@ -45,7 +46,7 @@ const AddUser = (props) => {
     }
 
     return (
-        <div>
+        <Wrapper>
             {error && <ErrorModal onConfirm={errorHandler} title={error.title} message={error.message}/>}
             <Card className="input">
                 <form onSubmit={addUserHandler}>
@@ -66,7 +67,7 @@ const AddUser = (props) => {
                     <Button type="submit">Add User</Button>
                 </form>
             </Card>
-        </div>
+        </Wrapper>
 
     )
 }
