819bf90dec55bd5b95f2c1e90857f2ef2b45af49
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2021-11-01 13:50:04 +0100

    Mini-project 02: Managing a list of users via State

--- mini-app-01/mini-app-02/src/App.js
+++ mini-app-01/mini-app-02/src/App.js
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import AddUser from "./components/Users/AddUser";
 import UsersList from "./components/Users/UsersList";
 
 function App() {
-  return (
-    <div>
-      <AddUser/>
-      <UsersList users={[]}/>
-    </div>
-  );
+    const [usersList, setUsersList] = useState([])
+
+    const addUserHandler = (username, userAge) => {
+        setUsersList((prevUsersList) => {
+            return [...prevUsersList,
+                { id: Math.random().toString() ,name: username, age: userAge}]
+        })
+    }
+    return (
+        <div>
+            <AddUser onAddUser={addUserHandler}/>
+            <UsersList users={usersList}/>
+        </div>
+    );
 }
 
 export default App;
