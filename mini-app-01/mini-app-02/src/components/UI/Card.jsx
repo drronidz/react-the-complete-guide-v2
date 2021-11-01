157d33aab92b489fd910482bb7336bac7cf7b0c3
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2021-11-01 12:42:52 +0100

    Mini-project 02: Adding Validation & Resetting Logic ...

--- mini-app-01/mini-app-02/src/components/UI/Card.jsx
+++ mini-app-01/mini-app-02/src/components/UI/Card.jsx
@@ -4,7 +4,7 @@ import  './Card.css'
 
 const Card = (props) => {
     return (
-        <div className={"card" + " " + props.className}>
+        <div className={"card " + props.className}>
             {props.children}
         </div>
     )
