e42ef94a8a3556bcf8a157db770ca87af2891a42
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2021-11-05 15:43:49 +0100

    Mini-project 03: -Using React Context API (via useContext Hook)

--- mini-app-01/mini-app-03/src/components/MainHeader/Navigation.js
+++ mini-app-01/mini-app-03/src/components/MainHeader/Navigation.js
@@ -1,37 +1,32 @@
-import React from 'react';
+import React, {useContext} from 'react';
 
 import AuthContext from "../store/auth-context";
 import classes from './Navigation.module.css';
 
 const Navigation = (props) => {
-  return (
-      <AuthContext.Consumer>
-          { (context) => {
-              return (
-                  <nav className={classes.nav}>
-                      <ul>
-                          {context.isLoggedIn && (
-                              <li>
-                                  <a href="/">Users</a>
-                              </li>
-                          )}
-                          {context.isLoggedIn && (
-                              <li>
-                                  <a href="/">Admin</a>
-                              </li>
-                          )}
-                          {context.isLoggedIn && (
-                              <li>
-                                  <button onClick={context.onLogout}>Logout</button>
-                              </li>
-                          )}
-                      </ul>
-                  </nav>
-              )
-          }}
-      </AuthContext.Consumer>
+    const context = useContext(AuthContext)
 
-  );
-};
+    return (
+        <nav className={classes.nav}>
+            <ul>
+                {context.isLoggedIn && (
+                    <li>
+                        <a href="/">Users</a>
+                    </li>
+                )}
+                {context.isLoggedIn && (
+                    <li>
+                        <a href="/">Admin</a>
+                    </li>
+                )}
+                {context.isLoggedIn && (
+                    <li>
+                        <button onClick={context.onLogout}>Logout</button>
+                    </li>
+                )}
+            </ul>
+        </nav>
+    )
+}
 
 export default Navigation;
