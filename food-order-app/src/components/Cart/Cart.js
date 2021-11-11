a74cc35fb239acd6713fad8fa8d96ec665122c85
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2021-11-11 23:59:39 +0100

    Making Items Removable ...

--- food-order-app/src/components/Cart/Cart.js
+++ food-order-app/src/components/Cart/Cart.js
@@ -13,11 +13,14 @@ const Cart = props => {
     const hasItems = cartContext.items.length > 0
 
     const cartItemRemoveHandler = id => {
-
+        cartContext.removeItem(id)
     }
 
     const cartItemAddHandler = item => {
-
+        cartContext.addItem({
+            ...item,
+            amount: 1
+        })
     }
 
     const cartItems =
--- food-order-app/src/store/CartProvider.js
+++ food-order-app/src/store/CartProvider.js
@@ -9,10 +9,10 @@ const defaultCartState = {
 
 const cartReducer = (state, action) => {
     if (action.type === 'ADD') {
-        const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount
+        const updatedTotalAmount =
+            state.totalAmount + action.item.price * action.item.amount
 
-        const existingCartItemIndex = state.items.findIndex(
-            item => item.id === action.item.id)
+        const existingCartItemIndex = state.items.findIndex((item) => item.id === action.item.id)
         const existingCartItem = state.items[existingCartItemIndex]
         let updatedItems
 
@@ -21,8 +21,9 @@ const cartReducer = (state, action) => {
                 ...existingCartItem,
                 amount: existingCartItem.amount + action.item.amount
             }
+
             updatedItems = [...state.items]
-            updatedItem[existingCartItemIndex] = updatedItem
+            updatedItems[existingCartItemIndex] = updatedItem
         } else {
             updatedItems = state.items.concat(action.item)
         }
@@ -32,6 +33,30 @@ const cartReducer = (state, action) => {
             totalAmount: updatedTotalAmount
         }
     }
+
+    if (action.type === 'REMOVE') {
+        const existingCartItemIndex = state.items.findIndex((item) => item.id === action.id)
+        const existingItem = state.items[existingCartItemIndex]
+        const updatedTotalAmount = state.totalAmount - existingItem.price
+        let updatedItems
+        if (existingItem.amount === 1) {
+            updatedItems = state.items.filter(item => item.id !== action.id)
+        }
+        else {
+            const updatedItem = {
+                ...existingItem,
+                amount: existingItem.amount - 1
+            }
+            updatedItems = [...state.items]
+            updatedItems[existingCartItemIndex] = updatedItem
+        }
+
+        return {
+            items: updatedItems,
+            totalAmount: updatedTotalAmount
+        }
+    }
+
     return defaultCartState
 }
 
@@ -46,7 +71,10 @@ const CartProvider = props => {
     }
 
     const removeItemFromCartHandler = id => {
-
+        dispatchCartAction( {
+            type: 'REMOVE',
+            id: id
+        })
     }
 
     const cartContext = {
