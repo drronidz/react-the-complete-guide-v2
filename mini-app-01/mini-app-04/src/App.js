9af2cad8b2257cbc29211b52fc98713e569634d0
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2021-11-12 22:49:19 +0100

    Sending an HTTP Request ...

--- mini-app-01/mini-app-04/src/App.js
+++ mini-app-01/mini-app-04/src/App.js
@@ -4,25 +4,27 @@ import MoviesList from './components/MoviesList';
 import './App.css';
 
 function App() {
+
   const [movies, setMovies] = useState([])
 
-  function fetchMovieHandler() {
-    fetch('https://swapi.dev/api/films/')
-        .then(response => {
-          return response.json()
-        })
-        .then(data => {
-          const transformedMovies = data.results.map(movieDATA => {
-            return {
-              id: movieDATA.episode_id,
-              title: movieDATA.title,
-              openingText: movieDATA.opening_crawl,
-              releaseDate: movieDATA.release_date
-            }
-          })
-          setMovies(data.results)
-        })
-  }
+    function fetchMovieHandler() {
+        fetch('https://swapi.dev/api/films/')
+            .then(response => {
+                return response.json()
+            })
+            .then(data => {
+                const transformedMovies = data.results.map(movieDATA => {
+                    return {
+                        id: movieDATA.episode_id,
+                        title: movieDATA.title,
+                        openingText: movieDATA.opening_crawl,
+                        releaseDate: movieDATA.release_date
+                    }
+                })
+                setMovies(data.results)
+            })
+    }
+
 
   return (
     <React.Fragment>
