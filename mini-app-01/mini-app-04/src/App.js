148247c933a7e8e0bfe4a3f10f1130a92e227fc6
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2021-11-12 23:52:23 +0100

    Using useEffect() For requests ...

--- mini-app-01/mini-app-04/src/App.js
+++ mini-app-01/mini-app-04/src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState, useCallback} from 'react';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
@@ -9,7 +9,9 @@ function App() {
     const [isLoading, setIsLoading] = useState(false)
     const [error, setError] = useState(null)
 
-    async function fetchMovieHandler() {
+
+
+    const fetchMovieHandler = useCallback(async () => {
         setIsLoading(true)
         setError(null)
 
@@ -37,7 +39,11 @@ function App() {
         }
         setIsLoading(false)
 
-    }
+    }, [])
+
+    useEffect(() => {
+        fetchMovieHandler()
+    }, [fetchMovieHandler])
 
     let content = <p>Found no movies.</p>
 
