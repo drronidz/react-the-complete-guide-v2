1a3846137b5b77672eb58287515a7459a373c850
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2021-11-14 22:25:13 +0100

    React To Lost focus ...

--- mini-app-01/mini-app-06/src/components/SimpleInput.js
+++ mini-app-01/mini-app-06/src/components/SimpleInput.js
@@ -17,6 +17,10 @@ const SimpleInput = (props) => {
         setEnteredName(event.target.value)
     }
 
+    const nameInputBlurHandler = event => {
+        setEnteredNameTouched(true)
+    }
+
     const formSubmissionHandler = event => {
         event.preventDefault()
 
@@ -50,6 +54,7 @@ const SimpleInput = (props) => {
                     type='text'
                     id='name'
                     onChange={nameInputChangeHandler}
+                    onBlur={nameInputBlurHandler}
                     value={enteredName}
                 />
                 {nameInputIsInvalid && <p className="error-text">Name must not be empty.</p>}
