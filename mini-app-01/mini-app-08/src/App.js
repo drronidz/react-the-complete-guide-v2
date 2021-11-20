160ef837242bae31ade1c8de1ac494a88ec535bc
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2021-11-20 23:10:27 +0100

    Handling Http States & Feedback with Redux ...

--- mini-app-01/mini-app-08/src/App.js
+++ mini-app-01/mini-app-08/src/App.js
@@ -1,25 +1,75 @@
-import React, {useEffect} from "react";
-import {useSelector} from "react-redux";
+import React, {Fragment, useEffect, useState} from "react";
+import {useSelector, useDispatch} from "react-redux";
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
+import uiSlice from "./store/ui-slice";
+import Notification from "./components/UI/Notification";
+
+let isInitial = true
 
 function App() {
+    const dispatch = useDispatch()
     const showCart = useSelector(state => state.ui.cartIsVisible)
     const cart = useSelector((state) => state.cart)
+    const notification = useSelector((state => state.ui.notification))
 
     useEffect(() => {
-        fetch('https://react-http-fe1b6-default-rtdb.firebaseio.com/cart.json', {
-            method: 'PUT',
-            body: JSON.stringify(cart)
+        const sendCartDATA = async () => {
+            dispatch(uiSlice.actions.showNotification({
+                status: 'pending',
+                title: 'Sending ...',
+                message: 'Sending Cart DATA!'
+            }))
+            const response = await fetch('https://react-http-fe1b6-default-rtdb.firebaseio.com/cart.json', {
+                method: 'PUT',
+                body: JSON.stringify(cart)
+            })
+
+            if (!response.ok) {
+                throw new Error('Sending cart DATA failed.')
+
+            }
+
+            const responseDATA = await response.json()
+
+            dispatch(uiSlice.actions.showNotification({
+                status: 'success',
+                title: 'Success!',
+                message: 'Cart DATA sent successfully!'
+            }))
+        }
+
+        if (isInitial) {
+            isInitial = false
+            return
+        }
+
+        sendCartDATA().catch((error) => {
+            dispatch(
+                uiSlice.actions.showNotification({
+                    status: 'error',
+                    title: 'Error!',
+                    message: 'Sending cart DATA failed!'
+                })
+            )
         })
-    }, [cart])
+    }, [cart, dispatch])
 
     return (
-        <Layout>
-            {showCart && <Cart/>}
-            <Products/>
-        </Layout>
+        <Fragment>
+            {notification &&
+            <Notification
+                status={notification.status}
+                title={notification.title}
+                message={notification.message}
+            />}
+            <Layout>
+                {showCart && <Cart/>}
+                <Products/>
+            </Layout>
+        </Fragment>
+
     );
 }
 
