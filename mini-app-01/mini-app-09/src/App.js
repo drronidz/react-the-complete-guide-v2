e273b7c9770f61643f71e64259dc5bd4c9d2d840
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2021-11-22 21:42:17 +0100

    Working with Links ...

--- mini-app-01/mini-app-09/src/App.js
+++ mini-app-01/mini-app-09/src/App.js
@@ -2,14 +2,18 @@ import {Route} from 'react-router-dom'
 import Welcome from "./pages/Welcome";
 import Products from "./pages/Products";
 import {Routes} from "react-router";
+import MainHeader from "./components/MainHeader";
 
 function App() {
     return (
         <div>
-            <Routes>
-                <Route path="/welcome" element={ <Welcome/> } />
-                <Route path="/products" element={ <Products/> } />
-            </Routes>
+            <MainHeader/>
+            <main>
+                <Routes>
+                    <Route path="/welcome" element={ <Welcome/> } />
+                    <Route path="/products" element={ <Products/> } />
+                </Routes>
+            </main>
         </div>
     );
 }
