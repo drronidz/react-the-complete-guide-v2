7fa549934e72e7732c6534d3f896812631e95c75
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2021-11-22 22:30:06 +0100

    Working with NavLinks ...

--- mini-app-01/mini-app-09/src/components/MainHeader.js
+++ mini-app-01/mini-app-09/src/components/MainHeader.js
@@ -1,19 +1,26 @@
-import  { Link } from "react-router-dom";
+import {NavLink} from "react-router-dom";
+
+import classes from './MainHeader.module.css'
 
 
 const MainHeader = () => {
-    return <header>
-        <nav>
-            <ul>
-                <li>
-                    <Link to="/welcome">Welcome</Link>
-                </li>
-                <li>
-                    <Link to="/products">Products</Link>
-                </li>
-            </ul>
-        </nav>
-    </header>
+    return (
+        <header className={classes.header}>
+            <nav>
+                <ul>
+                    <li>
+                        <NavLink className={(navData) => navData.isActive ? `${classes.active}` : `` } to="/welcome" >
+                            Welcome
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink className={(navData) => navData.isActive ? `${classes.active}` : `` } to="/products" >
+                            Products
+                        </NavLink>
+                    </li>
+                </ul>
+            </nav>
+        </header>)
 }
 
 export default MainHeader
