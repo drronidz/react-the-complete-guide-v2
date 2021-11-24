9ff68df32f2667cbf8804a582fb5e5cf9f486ddf
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2021-11-24 14:41:29 +0100

    Defining & Using Routes ... (V5)

--- mini-app-01/react-router-v5/src/App.js
+++ mini-app-01/react-router-v5/src/App.js
@@ -1,9 +1,21 @@
+import Route from "react-router-dom/es/Route";
+import Welcome from "./components/Welcome";
+import Products from "./components/Products";
+
 function App() {
-  return (
-    <div>
-      <h2>Let's get started!</h2>
-    </div>
-  );
+    return (
+        <div>
+            <Route path="/welcome">
+                <Welcome/>
+            </Route>
+            <Route path="/products">
+                <Products/>
+            </Route>
+        </div>
+    );
 }
 
 export default App;
+
+// our-domaine.com / welcome => Welcome Component ...
+// our-domaine.com / products => Products Component ...
