8e793a352d8be1c827c0401c9fa89ffcc3fcc6b8
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2021-11-24 15:14:04 +0100

    Using Switch & exact For Configuring Routes ... (V5)

--- mini-app-01/react-router-v5/src/App.js
+++ mini-app-01/react-router-v5/src/App.js
@@ -3,21 +3,24 @@ import Welcome from "./Pages/Welcome";
 import Products from "./Pages/Products";
 import MainHeader from "./components/MainHeader";
 import ProductDetail from "./Pages/ProductDetail";
+import Switch from "react-router-dom/es/Switch";
 
 function App() {
     return (
         <div>
             <MainHeader/>
             <main>
-                <Route path="/welcome">
-                    <Welcome/>
-                </Route>
-                <Route path="/pr oducts">
-                    <Products/>
-                </Route>
-                <Route path="/product-detail/:productId">
-                    <ProductDetail/>
-                </Route>
+                <Switch>
+                    <Route path="/welcome">
+                        <Welcome/>
+                    </Route>
+                    <Route path="/products" exact>
+                        <Products/>
+                    </Route>
+                    <Route path="/products/:productId">
+                        <ProductDetail/>
+                    </Route>
+                </Switch>
             </main>
         </div>
     );
--- mini-app-01/react-router-v5/src/Pages/Products.js
+++ mini-app-01/react-router-v5/src/Pages/Products.js
@@ -1,12 +1,22 @@
+import {Link} from "react-router-dom";
+
 const Products = () => {
     return (
         <section>
             <h1> The Products Page...</h1>
             <ul>
-                <li>A BOOK</li>
-                <li>A CARPET</li>
-                <li>A TOY</li>
-                <li>An Online Course</li>
+                <li>
+                    <Link to='/products/p1'>A BOOK</Link>
+                </li>
+                <li>
+                    <Link to='/products/p2'>A CARPET</Link>
+                </li>
+                <li>
+                    <Link to='/products/p3'>A TOY</Link>
+                </li>
+                <li>
+                    <Link to='/products/p4'>An Online Course</Link>
+                </li>
             </ul>
         </section>
 
