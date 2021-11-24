ee8ae84f8e2d544e0361383238ae9c52a2a29e74
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2021-11-24 21:51:22 +0100

    Implementing Programmatic Navigation ... (V5)

--- mini-app-01/react-quotes-v5/src/pages/NewQuote.js
+++ mini-app-01/react-quotes-v5/src/pages/NewQuote.js
@@ -1,11 +1,17 @@
 import QuoteForm from "../components/quotes/QuoteForm";
+import { useHistory } from 'react-router-dom'
 
 const NewQuote = () => {
+    const history = useHistory()
+
     const addQuoteHandler= quoteDATA => {
         console.log(quoteDATA)
+
+
+        history.push('/quotes')
     }
 
-    return <QuoteForm onAddQuote={addQuoteHandler()}/>
+    return <QuoteForm onAddQuote={addQuoteHandler}/>
 }
 
 export default NewQuote
